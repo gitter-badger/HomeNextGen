75afdb8879d0a91ca1e9e2d1223c4dcdfcfa780f
kteza1 <k.teza1@gmail.com>
2015-06-22 10:44:13 +0530

    modular openwrt automatic setup script

--- openwrt_setup.py
+++ openwrt_setup.py
@@ -2,6 +2,14 @@ import subprocess
 import re
 import shutil
 
+try:
+    import pytoml as toml
+except:
+    import pip
+    pip.main(['install', 'pytoml'])
+    import pytoml as toml
+
+
 #NOTE: After 'feeds install', '.config' is reflecting the installs only after saving the 'menuconfig'. So save after running the script first
 #NOTE: Rerun this script
 #NOTE: Make sure you do 'make menuconfig' and save to enable dependent packages on the packages that you have enabled
@@ -21,17 +29,17 @@ import shutil
 #Utilities:
 	#bluez-utils
 
-config_file = './openwrt/.config'
 
 def package_enable_disable(package_name, option):
-
     shutil.copyfile('./openwrt/.config', './openwrt/.oldconfig')
 
     package = 'CONFIG_PACKAGE_' + package_name
-    with open(config_file, 'r') as f, open('./openwrt/.newconfig', 'w') as new_config:
+    package = package.encode('utf-8')
+
+    with open('./openwrt/.config', 'r') as f, open('./openwrt/.newconfig', 'w') as new_config:
         for line in f:
             package_select = re.split(' |=', line, 2)
-            if package in package_select: 
+            if package in package_select:
                 if option == 'y' and package_select[1] != 'y\n' and package_select[2] == 'is not set\n':
                     print('enabling` -->', package_name)
                     new_config.write(package + '=y\n')
@@ -39,31 +47,55 @@ def package_enable_disable(package_name, option):
                     print('disbling -->', package_name)
                     new_config.write('# ' + package + ' is not set\n')
                 else:
-                    print('already ' + option)
+                    if option == 'y':
+                        print(package_name + ' already enabled')
+                    elif option == 'n':
+                        print(package_name + ' already disabled')
             else:
                 new_config.write(line)
     shutil.copyfile('./openwrt/.newconfig', './openwrt/.config')
 
 
-
-feeds_util = './openwrt/scripts/feeds '
-
-update = feeds_util + 'update -a'
-
-basic = "luci file "
-ble = "libexpat bluez-utils bluez-libs "
-
-packages_install = feeds_util + 'install ' + basic + ble 
-
-for arg in [update, packages_install]:
-    popen = subprocess.Popen(arg.split(), stdout=subprocess.PIPE)
+def execute_command(command):
+    popen = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
     popen.wait()
     output = popen.stdout.read()
     print(output)
 
+def feeds_update():
+    feeds_update_command = "./openwrt/scripts/feeds update -a"
+    execute_command(feeds_update_command)
+    
 
-others = "kmod-usb-core kmod-usb-ohci kmod-usb-storage kmod-usb2 kmod-bluetooth kmod-bluetooth_6lowpan block-mount"
+def package_install(package):
+    print('@@@@@@ installing ' + package + ' @@@@@@')
+    package_install_command = './openwrt/scripts/feeds install ' + package
+    execute_command(package_install_command)
 
-for package in basic.split() + ble.split() + others.split():
-    package_enable_disable(package, 'y')
 
+#feeds_update()
+
+
+with open('packages.toml', 'rb') as f:
+    config = toml.load(f)
+    for section in config:
+        if section == 'install_enable':
+            categories = config[section]
+            for category in categories:
+                packages = categories[category]
+                for package in packages:
+                    package_install(package)
+                # package won't be available in .config first time after install.
+                # menuconfig should be save for these sections to reflect in .config
+                # do something here to simulate that
+            for category in categories:
+                packages = categories[category]
+                for package in packages:
+                    package_enable_disable(package, 'y')
+
+        if section == 'enable':
+            categories = config[section]
+            for category in categories:
+                packages = categories[category]
+                for package in packages:
+                   package_enable_disable(package, 'y')
